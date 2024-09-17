a250370c58278df9df2d6688fc35d9e1790bfd87
kiruha17 <wangterry36b@gmail.com>
2024-09-17 12:01:41 -0400

    Start Polynomial.java

---
+++ Polynomial.java
@@ -0,0 +1,42 @@
+public class Polynomial {
+
+    double[] coefficients;
+
+    public Polynomial() {
+        coefficients = new double[1];
+        coefficients[0] = 0;
+
+    }
+
+    public Polynomial(double[] coeffs) {
+        coefficients = new double[coeffs.length];
+
+        for (int i = 0; i < coeffs.length; i++) {
+            coefficients[i] = coeffs[i];
+        }
+    }
+
+    public Polynomial add(Polynomial toAdd) {
+        int maxLength = Math.max(this.coefficients.length, toAdd.coefficients.length);
+        int minLength = Math.min(this.coefficients.length, toAdd.coefficients.length);
+
+        double[] result = new double[maxLength]; // [] not ()
+        for (int i = 0; i < this.coefficients.length; i++) {
+            result.coefficients[i] += this.coefficients[i];
+        }
+        for (int i = 0; i < toAdd.coefficients.length; i++) {
+            result.coefficients[i] += toAdd.coefficients[i];
+        }
+
+        return new Polynomial(result);
+    }
+
+    //Done 
+
+    public evaulate(double x) {
+        double result = this.coefficients[0];
+        for (int i = 1; i < this.length; i++) {
+            result += Math.pow(this.coefficients[i], i);
+        }
+    }
+}
